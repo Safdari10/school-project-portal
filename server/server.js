04081f9167bc9c99e074c40cb878f1d4a06100c8
Safdari10 <zakersafdari@gmail.com>
2026-05-12 22:30:28 +1200

    style: format code in server.js for consistency

--- server/server.js
+++ server/server.js
@@ -1,26 +1,24 @@
-const express = require("express")
-require("dotenv").config()
-const cors = require("cors")
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
 
-const loginSignupRouter = require("./router/loginSignupRoute")
-const studentContentRouter = require("./router/StudentContentRoute")
+const loginSignupRouter = require("./router/loginSignupRoute");
+const studentContentRouter = require("./router/StudentContentRoute");
 
-
-
-const app = express()
+const app = express();
 
 //middleware
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 // use the created routes
 
-app.use('/api', loginSignupRouter)
-app.use("/api", studentContentRouter)
+app.use("/api", loginSignupRouter);
+app.use("/api", studentContentRouter);
 
 // create port
 
-const port = process.env.PORT 
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
-    console.log(`Server Listening on http://localhost:${port}`)
-})
+  console.log(`Server Listening on http://localhost:${port}`);
+});
