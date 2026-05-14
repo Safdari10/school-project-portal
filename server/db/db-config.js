71b807bfc278dea4385c1dabce9c67b541fbc808
Safdari10 <zakersafdari@gmail.com>
2026-05-14 21:50:17 +1200

    style: improve formatting and consistency in db-config.js

--- server/db/db-config.js
+++ server/db/db-config.js
@@ -1,28 +1,26 @@
-
-const mysql = require("mysql2/promise")
+const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-   host: process.env.MYSQL_HOST,
-   user: process.env.MYSQL_USER,
-   password: process.env.MYSQL_PASSWORD,
-   database: process.env.MYSQL_DATABASE,
-   waitForConnections: true,
-   connectionLimit: 10,
-   queueLimit: 5,
-})
-
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD || process.env.MYSQL_PASS,
+  database: process.env.MYSQL_DATABASE,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 5,
+});
 
 // -- wrapper function for executing parameterized queries --
 
 const query = async (sql, params) => {
-   try {
-      const [results] = await pool.execute(sql, params)
-      return results;
-   } catch (error) {
-      throw new Error('Database query error:' + error.message)
-   }
-}
+  try {
+    const [results] = await pool.execute(sql, params);
+    return results;
+  } catch (error) {
+    throw new Error("Database query error:" + error.message);
+  }
+};
 
 module.exports = {
-   query
-}
+  query,
+};
